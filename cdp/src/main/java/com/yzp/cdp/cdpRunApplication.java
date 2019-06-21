57b59af7341406142f260208ee2e5026169b14a9
YaoZhenPeng <18065220862@163.com>
2019-06-21 22:06:45 +0800

    添加测试代码

---
+++ cdp/src/main/java/com/yzp/cdp/cdpRunApplication.java
@@ -0,0 +1,12 @@
+package com.yzp.cdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cdpRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(cdpRunApplication.class,args);
+    }
+}
