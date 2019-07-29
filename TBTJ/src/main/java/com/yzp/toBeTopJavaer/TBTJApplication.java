fc29ef97f623e64fc4c3e5b3fa415c97aae77a4a
YaoZhenPeng <18065220862@163.com>
2019-07-30 00:24:56 +0800

    修改目录结构添加代码

--- TBTJ/src/main/java/com/yzp/toBeTopJavaer/TBTJApplication.java
+++ TBTJ/src/main/java/com/yzp/toBeTopJavaer/TBTJApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class cdpRunApplication {
+public class TBTJApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(cdpRunApplication.class,args);
+        SpringApplication.run(TBTJApplication.class,args);
     }
 }
